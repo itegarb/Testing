bfd2b08e8f0aba7c7df1593a8ab394145d15480c
itegarb <itbudiyarto@gmail.com>
2015-08-26 23:44:52 +0700

    Update exercise2.cpp

---
+++ exercise2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main (){
+    int a=2;
+    int b=3;
+    int i=1;
+    int n=1;
+
+    while (i<b){
+        n=n*a;
+        i=i+1;
+    }
+    cout<<"HASIL="<<n<<endl<<endl;
+getch ();
+    }
